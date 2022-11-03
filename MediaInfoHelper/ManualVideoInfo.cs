b0624a30d077d2ae5dec9a7ac07bf57b0eda88e5
DJDoena <github@doena-soft.de>
2022-11-03 22:19:17 +0100

    2.0.5

---
+++ MediaInfoHelper/ManualVideoInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace DoenaSoft.MediaInfoHelper.Youtube
+{
+    /// <summary>
+    /// XML structure to represent a manual video entry.
+    /// </summary>
+    public sealed class ManualVideoInfo
+    {
+        /// <summary />
+        public string Title { get; set; }
+
+        /// <summary />
+        public string Note { get; set; }
+
+        /// <summary />
+        public uint RunningTime { get; set; }
+    }
+}
