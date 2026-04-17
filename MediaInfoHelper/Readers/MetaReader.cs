2dcf013367173e9270808ad336ca229eb6874e8d
DJ Doena <djdoena@outlook.com>
2026-04-17 12:56:39 +0200

    3.2.3

--- MediaInfoHelper/Readers/MetaReader.cs
+++ MediaInfoHelper/Readers/MetaReader.cs
@@ -1,5 +1,4 @@
-﻿using System.Xml;
-using DoenaSoft.MediaInfoHelper.DataObjects.AudioBookMetaXml;
+﻿using DoenaSoft.MediaInfoHelper.DataObjects.AudioBookMetaXml;
 using DoenaSoft.MediaInfoHelper.DataObjects.FFProbeMetaXml;
 using DoenaSoft.MediaInfoHelper.DataObjects.VideoMetaXml;
 using DoenaSoft.ToolBox.Generics;
@@ -12,16 +11,7 @@ public static class MetaReader
     /// <summary/>
     public static VideoMeta ReadVideoMetaDocument(string fileName)
     {
-        using var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-        using var xmlReader = new XmlTextReader(stream);
-
-        var settings = new XmlReaderSettings()
-        {
-            DtdProcessing = DtdProcessing.Parse,
-        };
-
-        var doc = XmlSerializer<VideoInfoDocument>.Deserialize(xmlReader);
+        var doc = XsltSerializer<VideoInfoDocument>.Deserialize(fileName);
 
         return doc.VideoInfo;
     }
@@ -37,16 +27,7 @@ public static class MetaReader
     /// <summary/>
     public static AudioBookMeta ReadAudioBookMetaDocument(string fileName)
     {
-        using var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-        using var xmlReader = new XmlTextReader(stream);
-
-        var settings = new XmlReaderSettings()
-        {
-            DtdProcessing = DtdProcessing.Parse,
-        };
-
-        var doc = XmlSerializer<AudioBookDocument>.Deserialize(xmlReader);
+        var doc = XsltSerializer<AudioBookDocument>.Deserialize(fileName);
 
         return doc.Mp3Meta;
     }
